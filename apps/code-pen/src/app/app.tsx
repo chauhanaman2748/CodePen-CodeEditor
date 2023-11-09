24eb3eae14dda7d26b7e65d19b440936aee98e7d
My Name <spyman2748@gmail.com>
2023-11-08 21:41:30 -0500

    Setup complete

---
+++ apps/code-pen/src/app/app.tsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
